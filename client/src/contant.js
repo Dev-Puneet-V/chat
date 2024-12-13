8f5077e5502d661afe1b240491182dbbdb4e656c
puneet <imhtcy312@gmail.com>
2024-12-13 15:19:05 +0530

    deploy

--- client/src/contant.js
+++ client/src/contant.js
@@ -1,3 +1,3 @@
-const API_URL = "http://localhost:3000";
-// const API_URL = "https://chat-k7m6.onrender.com";
+// const API_URL = "http://localhost:3000";
+const API_URL = "https://chat-k7m6.onrender.com";
 export { API_URL };
